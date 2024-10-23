c29795e66e7aee76c18d629563a73f577f5703e8
Rochim Azis Rajasa <rochimazis82927@gmail.com>
2024-10-23 23:00:36 +0700

    Hmm

--- sesi_6.py
+++ sesi_6.py
@@ -1,12 +1,12 @@
 import pygame as pg
 import random as ran
 
+
 # Inisialisasi pygame
-pg.init
+pg.init()
 
 # Membuat jendela game
-WIDTH = 800
-HEIGHT = 600
+WIDTH, HEIGHT = 800, 600
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('Dodge The Enemies')
 
@@ -24,7 +24,7 @@ BLUE = (0,0,255)
 FPS = 60
 clock = pg.time.Clock()
 
-# Kelas untuk player
+# Class untuk Player
 class Player(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -46,30 +46,87 @@ class Player(pg.sprite.Sprite):
         if keys[pg.K_DOWN] and self.rect.bottom < HEIGHT:
             self.rect.y += self.speed
 
-# Inisialisasi Sprite Game
+# Class untuk Enemies
+class Enemies (pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pg.Surface((50,50))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = ran.randint(0, WIDTH - 75)
+        self.rect.y = ran.randint(-100,-50)
+        self.speed = ran.randint(10,15)
+    
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.top:
+            self.rect.x = ran.randint(0, WIDTH - 75)
+            self.rect.y = ran.randint(-100,-50)
+            self.speed = ran.randint(2,6)
+
+# Inisialisasi Sprite Group Enemy
 all_sprites = pg.sprite.Group()
+enemies = pg.sprite.Group()
 
 # Membuat Player
 player = Player()
 all_sprites.add(player)
 
+# Membuat Enemies
+for i in range(5): # Ada 5 musuh di awal
+    enemy = Enemies()
+    all_sprites.add(enemy)
+    enemies.add(enemy)
+
+# Membuat Score
+score = 0
+start = pg.time.get_ticks()
+
 # Loop Game
 run = True
+game_over = False
 while run:
+    clock.tick(FPS)
+
     # Menutup game jika tekan x
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
 
+    # Logika Game Over
+    if not game_over:
+        # Update semua sprites
+        all_sprites.update()
+
+        # Mengecek apakah player hit enemies
+        hits = pg.sprite.spritecollide(player, enemies, False)
+        if hits:
+            game_over = True
+            game_over_time = (pg.time.get_ticks() - start) // 1000 # Menghitung waktu bertahan hidup
+
+        # Menghitung Score Akhir
+        score = (pg.time.get_ticks()-start) // 1000
+
     # Menampilakan background
     screen.blit(image, (0,0))
 
     # Menggambar objek
-    all.sprite
+    all_sprites.draw(screen)
+    
+
+    # Menampilkan score
+    font = pg.font.SysFont(None,30)
+    text = font.render(f"score: {score}", True, WHITE)
+    screen.blit(text, (10,10))
+
+    # Menampilkan game over
+    if game_over:
+        over_text = font.render(f"Game Over! Final Score: {game_over_time}", True, WHITE)
+        screen.blit(over_text, (WIDTH // 2 - 150, HEIGHT // 2))
 
     # Update tampilan layar
     pg.display.flip()
     pg.display.update() 
-
+    
 
 pg.quit()
